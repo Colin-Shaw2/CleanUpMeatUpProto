f529aeea1c44710c16cbb767b22079ff19541ba2
Colin-Shaw2 <colin.shaw2@uoit.net>
2019-11-15 15:22:51 -0500

    navigation and assets now work

---
+++ clean_up_meet_p/lib/pages/detailsPage.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Details Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+          ],
+        ),
+      ),
+      
+    );
+  }
+
+}
+
